b139661ed96260fc204ea5b4f10d6343a0cea759
Ananya <ananyakumari0710@gmail.com>
2024-07-08 05:50:45 +0000

    no change

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+// alert (3+3) // we are using nodejs, not browser
+
+console.log(3+3);  //code readability should be high
+console.log("Ananya")
+
+let name= "ananya"
+let age = 18
+let isloggedin = false
+let state;
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standlone value
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof null); //output object
+console.log(typeof undefined);  //output undefined
+console.log(typeof "Ananya");   //string
